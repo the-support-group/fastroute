1d3835d2a8dc68b97b23669ffbe62d6c81b4b062
Mark Locker <mark.locker@easyfundraising.org.uk>
2014-09-03 21:14:29 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# FastRoute
+A [FastRoute](https://github.com/nikic/FastRoute) implementation for Air's [routing library](https://github.com/air-php/routing).
+
+## Installation
+Installation via [Composer](https://getcomposer.org/) is recommended.
+
+    "require": {
+        "air-php/fastroute": "dev-master"
+    }
---
+++ src/FastRoute.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Air\Routing\FastRoute;
+
+use Air\Routing\Router\Router;
+use Air\HTTP\Request\RequestInterface;
+use FastRoute\RouteCollector;
+use FastRoute\Dispatcher;
+use Air\Routing\ResolvedRequest\ResolvedRequest;
+use Exception;
+use OutOfRangeException;
+
+class FastRoute extends Router
+{
+    public function route(RequestInterface $request)
+    {
+        // Add routes to the route collector.
+        $dispatcher = \FastRoute\simpleDispatcher(
+            function (RouteCollector $collector) {
+                foreach ($this->routes as $route) {
+                    $collector->addRoute(
+                        $route->getRequestType(),
+                        $route->getUri(),
+                        serialize($route)
+                    );
+                }
+            }
+        );
+
+        // Dispatch the route.
+        $route_info = $dispatcher->dispatch($request->getMethod(), $request->getUriPath());
+
+        // Handle the route depending on the response type.
+        switch ($route_info[0]) {
+            // Route not found.
+            case Dispatcher::NOT_FOUND:
+
+                throw new OutOfRangeException("No route matched the given URI.");
+
+            // Method not allowed for the specified route.
+            case Dispatcher::METHOD_NOT_ALLOWED:
+
+                throw new Exception("Method not allowed.");
+
+            // Route found.
+            case Dispatcher::FOUND:
+
+                $route = $route_info[1];
+                $params = $route_info[2];
+
+                return new ResolvedRequest($request, unserialize($route), $params);
+        }
+    }
+}
